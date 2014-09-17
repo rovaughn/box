807b399b2d88476925ff39a03070a6b934110fd7
Alec Newman <alecnwmn904@gmail.com>
2014-09-16 21:32:12 -0600

    Better maybe.

--- main.c
+++ main.c
@@ -10,23 +10,75 @@
   #error "The hash size is too small for the secretbox key"
 #endif
 
-/* Usage:
- *  box 'password'
- *  unbox 'password'
- */
-
 int print_usage(void) {
   fprintf(stderr, "Usage: box 'password'\n");
   fprintf(stderr, "   or  unbox 'password'\n");
   return 1;
 }
 
+typedef struct {
+  uint16_t len;
+  uint8_t  nonce[crypto_secretbox_NONCEBYTES];
+} block_header_t;
+
+typedef struct {
+  block_header_t header;
+  union {
+    uint8_t boxed_bytes[crypto_secretbox_ZEROBYTES + BLOCKSIZE];
+    struct {
+      uint8_t zero_bytes[crypto_secretbox_ZEROBYTES];
+      uint8_t unboxed_bytes[BLOCKSIZE];
+    };
+  };
+} block_t;
+
+#define MIN_BLOCK_SIZE (2 + crypto_secretbox_NONCEBYTES + crypto_secretbox_ZEROBYTES)
+
 void box_command(uint8_t *key) {
-  printf("Box\n");
+  block_t block;
+
+  while (1) {
+    memset(block.zero_bytes, 0, sizeof block.zero_bytes);
+
+    size_t nread = fread(block.unboxed_bytes, 1, sizeof block.unboxed_bytes, stdin);
+
+    if (nread == 0) {
+      break;
+    }
+
+    block.header.len = crypto_secretbox_ZEROBYTES + nread;
+
+    /* We'll see if the fact that the source and target buffers are the same
+     * causes a problem. */
+    crypto_secretbox(
+      block.boxed_bytes, block.boxed_bytes, crypto_secretbox_ZEROBYTES + nread,
+      block.header.nonce, key
+    );
+
+    fwrite(&block, sizeof block - (BLOCKSIZE - nread), 1, stdout);
+  }
 }
 
 void unbox_command(uint8_t *key) {
-  printf("Unbox\n");
+  block_t block;
+
+  while (1) {
+    fread(&block.header, sizeof block.header, 1, stdin);
+
+    if (ferror(stdin)) {
+      break;
+    }
+
+    fread(block.boxed_bytes, block.header.len, 1, stdin);
+
+    crypto_secretbox_open(
+        block.boxed_bytes, block.boxed_bytes, block.header.len,
+        block.header.nonce, key
+    );
+
+    /* Bad subtraction */
+    fwrite(block.unboxed_bytes, block.header.len - crypto_secretbox_ZEROBYTES, 1, stdout);
+  }
 }
 
 int main(int argc, char **argv) {
@@ -34,18 +86,18 @@ int main(int argc, char **argv) {
     return print_usage();
   }
 
-  char *command  = argv[0],
-       *password = argv[1];
+  unsigned char *command  = (unsigned char*)argv[0],
+                *password = (unsigned char*)argv[1];
 
   uint8_t key[crypto_hash_sha256_BYTES]; // Reserve hash_BYTES for the hash,
                                          // but only actually use the first
                                          // secretbox_BYTES.
 
-  crypto_hash_sha256(key, password, strlen(password));
+  crypto_hash_sha256(key, password, strlen((char*)password));
 
-  if (strcmp(command, "./box") == 0) {
+  if (strcmp((char*)command, "./box") == 0) {
     box_command(key);
-  } else if (strcmp(command, "unbox") == 0) {
+  } else if (strcmp((char*)command, "./unbox") == 0) {
     unbox_command(key);
   } else {
     return print_usage();
