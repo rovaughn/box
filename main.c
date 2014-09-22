0c64dc1b1ee9c22fff2c3689e7abc93ff195e25a
anewman <anewman@balihoo.com>
2014-09-22 17:17:41 -0600

    Broken.

--- main.c
+++ main.c
@@ -22,73 +22,107 @@ int print_usage(void) {
   return 1;
 }
 
-void box_command(uint8_t *key) {
-  while (1) {
-    struct {
-      uint8_t zero[crypto_secretbox_ZEROBYTES],
-              message[BLOCKSIZE];
-    } message = {{0}};
+uint8_t *read_stdin(size_t initialZero, size_t initialFree, size_t *used) {
+  size_t   bufferUsed = initialZero,
+           bufferFree = initialFree;
+  uint8_t *buffer     = malloc(bufferUsed + bufferFree);
 
-    size_t nread = fread(message.message, 1, BLOCKSIZE, stdin);
+  memset(buffer, 0, initialZero);
 
-    if (nread == 0) break;
+  for (;;) {
+    size_t nread = fread(&buffer[bufferUsed], 1, bufferFree, stdin);
 
-    struct {
-      uint16_t len;
-      uint8_t  nonce[crypto_secretbox_NONCEBYTES],
-               message[nread + BOX_EXTRA];
-    } outblock;
-
-    outblock.len = nread;
-    randombytes(outblock.nonce, sizeof outblock.nonce);
-
-    struct {
-      uint8_t zero[crypto_secretbox_BOXZEROBYTES],
-              message[nread + BOX_EXTRA];
-    } boxed_message;
-
-    crypto_secretbox((void*)&boxed_message, (void*)&message, sizeof boxed_message, outblock.nonce, key);
-    memcpy(outblock.message, boxed_message.message, sizeof outblock.message);
-
-    /* TODO check errors */
-    fwrite(&outblock, sizeof outblock, 1, stdout);
+    if (nread < bufferFree) {
+      bufferUsed += nread;
+      buffer      = realloc(buffer, bufferUsed);
+      goto done;
+    } else {
+      bufferUsed += bufferFree;
+      bufferFree  = bufferUsed / 2;
+      buffer      = realloc(buffer, bufferUsed + bufferFree);
+    }
   }
+
+done:
+  *used = bufferUsed;
+
+  return buffer;
+}
+
+void showb(uint8_t *ptr, size_t n) {
+  size_t i;
+  for (i = 0; i < n; ++i) {
+    fprintf(stderr, "%02x ", ptr[i]);
+  }
+  fprintf(stderr, "\n");
+}
+
+void box_command(uint8_t *key) {
+  size_t   bufferUsed;
+  uint8_t *buffer = read_stdin(crypto_secretbox_ZEROBYTES, 1024, &bufferUsed),
+           nonce[crypto_secretbox_NONCEBYTES];
+
+  struct {
+    union {
+      struct {
+        uint8_t padding[crypto_secretbox_ZEROBYTES - crypto_secretbox_NONCEBYTES],
+                nonce[crypto_secretbox_NONCEBYTES];
+      };
+      uint8_t zero[crypto_secretbox_ZEROBYTES];
+    };
+    uint8_t message[bufferUsed];
+  } outblock;
+
+  randombytes(nonce, sizeof nonce);
+  showb(nonce, sizeof nonce);
+  showb(buffer, bufferUsed);
+
+  crypto_secretbox(
+    (void*)outblock.zero, (void*)buffer,
+    bufferUsed, nonce, key
+  );
+
+  memcpy(outblock.nonce, nonce, sizeof nonce);
+
+  showb((void*)&outblock, sizeof outblock);
+
+  /* TODO check errors */
+  fwrite(outblock.nonce, sizeof outblock - sizeof outblock.padding, 1, stdout);
+
+  free(buffer);
 }
 
 void unbox_command(uint8_t *key) {
-  while (1) {
+  size_t bufferUsed;
+  union {
     struct {
-      uint16_t len;
-      uint8_t  nonce[crypto_secretbox_NONCEBYTES];
-    } block_header;
-
-    if (fread(&block_header, sizeof block_header, 1, stdin) == 0) {
-      break;
-    }
-
+      uint8_t nonce[crypto_secretbox_NONCEBYTES],
+              ciphertext[bufferUsed - crypto_secretbox_NONCEBYTES];
+    };
     struct {
-      uint8_t zero[crypto_secretbox_BOXZEROBYTES],
-              message[block_header.len + BOX_EXTRA];
-    } boxed_message;
+      uint8_t boxpad[crypto_secretbox_NONCEBYTES - crypto_secretbox_BOXZEROBYTES],
+              boxzero[crypto_secretbox_BOXZEROBYTES];
+    };
+  } *inblock = (void*)read_stdin(0, 1024, &bufferUsed);
 
-    memset(boxed_message.zero, 0, sizeof boxed_message.zero);
-
-    struct {
-      uint8_t zero[crypto_secretbox_ZEROBYTES],
-              message[block_header.len];
-    } message;
-
-    if (fread(boxed_message.message, sizeof boxed_message.message, 1, stdin) == 0) {
-      break;
-    }
-
-    if (crypto_secretbox_open((void*)&message, (void*)&boxed_message, sizeof boxed_message, block_header.nonce, key) == -1) {
-      fprintf(stderr, "Block failed to decrypt.\n");
-      exit(1);
-    }
-
-    fwrite(message.message, sizeof message.message, 1, stdout);
+  if (bufferUsed < crypto_secretbox_NONCEBYTES) {
+    fprintf(stderr, "The message is not long enough to contain a nonce, and is thus invalid.\n");
+    exit(1);
   }
+
+  uint8_t nonce[crypto_secretbox_NONCEBYTES];
+  struct {
+    uint8_t zero[crypto_secretbox_ZEROBYTES],
+            plaintext[bufferUsed - crypto_secretbox_NONCEBYTES - BOX_EXTRA];
+  } out;
+
+  memcpy(nonce, inblock->nonce, sizeof nonce);
+  memset(inblock->boxzero, 0, sizeof inblock->boxzero);
+  crypto_secretbox_open(out.zero, inblock->boxzero, sizeof out, nonce, key);
+
+  fwrite(out.plaintext, sizeof out.plaintext, 1, stdout);
+
+  free(inblock);
 }
 
 int main(int argc, char **argv) {
@@ -105,9 +139,9 @@ int main(int argc, char **argv) {
 
   crypto_hash_sha256(key, (unsigned char*)password, strlen(password));
 
-  if (strcmp(command, "box") == 0) {
+  if (strcmp(command, "./box") == 0) {
     box_command(key);
-  } else if (strcmp(command, "unbox") == 0) {
+  } else if (strcmp(command, "./unbox") == 0) {
     unbox_command(key);
   } else {
     return print_usage();
