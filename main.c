9af9b4fae4a9f69661bda92055b778a22d4ce7e6
Alec NEwman <alecnwmn904@gmail.com>
2016-11-01 12:22:16 -0400

    box beforenm

--- main.c
+++ main.c
@@ -64,33 +64,18 @@ void show(size_t len, uint8_t data[len]) {
     printf("\n");
 }
 
-void store_secret_key(const char *path, size_t len, const uint8_t key[len]) {
-    int fd = open(path, O_WRONLY, S_IRUSR);
+const mode_t public_mode = S_IRUSR;
+const mode_t secret_mode = S_IRUSR|S_IRGRP|S_IROTH;
 
+void store_key(const char *path, mode_t mode, const char *label, size_t len, const uint8_t key[len]) {
+    int fd = open(path, O_WRONLY|O_CREAT, mode);
     fatal(fd, "open");
 
-    static const char label[] = {'s', 'e', 'c', 'r', 'e', 't', ' '};
-    char buffer[sizeof label + 2*len + 1];
+    char buffer[strlen(label) + 2*len + 1];
 
-    memcpy(buffer, label, sizeof label);
-    to_hex(len, key, &buffer[sizeof label]);
-    buffer[sizeof label + 2*len] = '\n';
-
-    fatal(write(fd, buffer, sizeof buffer), "write");
-    fatal(close(fd), "close");
-}
-
-void store_public_key(const char *path, size_t len, const uint8_t key[len]) {
-    int fd = open(path, O_WRONLY, S_IRUSR|S_IRGRP|S_IROTH);
-
-    fatal(fd, "open");
-
-    static const char label[] = {'p', 'u', 'b', 'l', 'i', 'c', ' '};
-    char buffer[sizeof label + 2*len + 1];
-
-    memcpy(buffer, label, sizeof label);
-    to_hex(len, key, &buffer[sizeof label]);
-    buffer[sizeof buffer - 1] = '\n';
+    memcpy(buffer, label, strlen(label));
+    to_hex(len, key, &buffer[strlen(label)]);
+    buffer[strlen(label) + 2*len] = '\n';
 
     fatal(write(fd, buffer, sizeof buffer), "write");
     fatal(close(fd), "close");
@@ -157,14 +142,9 @@ void cmd_box_keypair(int argc, char *argv[argc]) {
     int c;
     while ((c = getopt(argc, argv, "p:s:")) != -1) {
         switch (c) {
-        case 'p':
-            public_keyfile = optarg;
-            break;
-        case 's':
-            secret_keyfile = optarg;
-            break;
-        default:
-            abort();
+        case 'p': public_keyfile = optarg; break;
+        case 's': secret_keyfile = optarg; break;
+        default: abort();
         }
     }
 
@@ -183,13 +163,13 @@ void cmd_box_keypair(int argc, char *argv[argc]) {
 
     crypto_box_keypair(pk, sk);
 
-    store_public_key(public_keyfile, sizeof pk, pk);
-    store_secret_key(secret_keyfile, sizeof sk, sk);
+    store_key(public_keyfile, public_mode, "public ", sizeof pk, pk);
+    store_key(secret_keyfile, secret_mode, "secret ", sizeof sk, sk);
 
     memset_s(sk, 0, sizeof sk);
 }
 
-void cmd_box_make(int argc, char *argv[argc]) {
+void cmd_box(int argc, char *argv[argc]) {
     const char *public_keyfile = NULL;
     const char *secret_keyfile = NULL;
     const char *message_file = NULL;
@@ -199,20 +179,11 @@ void cmd_box_make(int argc, char *argv[argc]) {
     char c;
     while ((c = getopt(argc, argv, "m:c:p:s:")) != -1) {
         switch (c) {
-        case 'p':
-            public_keyfile = optarg;
-            break;
-        case 's':
-            secret_keyfile = optarg;
-            break;
-        case 'm':
-            message_file = optarg;
-            break;
-        case 'c':
-            ciphertext_file = optarg;
-            break;
-        case '?':
-            abort();
+        case 'p': public_keyfile = optarg; break;
+        case 's': secret_keyfile = optarg; break;
+        case 'm': message_file = optarg; break;
+        case 'c': ciphertext_file = optarg; break;
+        default: abort();
         }
     }
     }
@@ -256,7 +227,7 @@ void cmd_box_make(int argc, char *argv[argc]) {
 
     crypto_box(&c[crypto_box_NONCEBYTES], m, mlen, c, pk, sk);
 
-    int ciphertext_fd = open(ciphertext_file, O_WRONLY, S_IRUSR|S_IRGRP|S_IROTH);
+    int ciphertext_fd = open(ciphertext_file, O_WRONLY|O_CREAT, S_IRUSR|S_IRGRP|S_IROTH);
     fatal(ciphertext_fd, "open");
 
     // TODO: This unnecessarily includes the zero bytes.
@@ -274,22 +245,13 @@ void cmd_box_open(int argc, char *argv[argc]) {
 
     {
     char c;
-    while ((c = getopt(argc, argv, "m:c:p:s:")) != -1) {
+    while ((c = getopt(argc, argv, "p:s:m:c:")) != -1) {
         switch (c) {
-        case 'p':
-            public_keyfile = optarg;
-            break;
-        case 's':
-            secret_keyfile = optarg;
-            break;
-        case 'm':
-            message_file = optarg;
-            break;
-        case 'c':
-            ciphertext_file = optarg;
-            break;
-        case '?':
-            abort();
+        case 'p': public_keyfile = optarg; break;
+        case 's': secret_keyfile = optarg; break;
+        case 'm': message_file = optarg; break;
+        case 'c': ciphertext_file = optarg; break;
+        default: abort();
         }
     }
     }
@@ -340,7 +302,189 @@ void cmd_box_open(int argc, char *argv[argc]) {
         exit(1);
     }
 
-    int message_fd = open(message_file, O_WRONLY, S_IRUSR);
+    int message_fd = open(message_file, O_WRONLY|O_CREAT, S_IRUSR);
+    fatal(message_fd, "open");
+
+    // TODO: This unnecessarily includes the zero bytes.
+    fatal(write(message_fd, &m[crypto_box_ZEROBYTES], sizeof m - crypto_box_ZEROBYTES), "write");
+    fatal(close(message_fd), "close");
+}
+
+void cmd_box_beforenm(int argc, char *argv[argc]) {
+    const char *public_keyfile = NULL;
+    const char *secret_keyfile = NULL;
+    const char *keyfile = NULL;
+
+    {
+        char c;
+        while ((c = getopt(argc, argv, "p:s:k:")) != -1) {
+            switch (c) {
+            case 'p': public_keyfile = optarg; break;
+            case 's': secret_keyfile = optarg; break;
+            case 'k': keyfile = optarg; break;
+            default: abort();
+            }
+        }
+    }
+
+    if (!public_keyfile) {
+        fprintf(stderr, "Public keyfile must be specified with -p.\n");
+        exit(1);
+    }
+
+    if (!secret_keyfile) {
+        fprintf(stderr, "Secret keyfile must be specified with -s.\n");
+        exit(1);
+    }
+
+    if (!keyfile) {
+        fprintf(stderr, "Output keyfile must be specified with -k.\n");
+        exit(1);
+    }
+
+    uint8_t pk[crypto_box_PUBLICKEYBYTES];
+    uint8_t sk[crypto_box_SECRETKEYBYTES];
+
+    load_key(public_keyfile, "public ", sizeof pk, pk);
+    load_key(secret_keyfile, "secret ", sizeof sk, sk);
+
+    uint8_t k[crypto_box_BEFORENMBYTES];
+
+    crypto_box_beforenm(k, pk, sk);
+
+    store_key(keyfile, secret_mode, "beforenm ", sizeof k, k);
+}
+
+void cmd_box_afternm(int argc, char *argv[argc]) {
+    const char *keyfile = NULL;
+    const char *message_file = NULL;
+    const char *ciphertext_file = NULL;
+
+    {
+        char c;
+        while ((c = getopt(argc, argv, "k:m:c:")) != -1) {
+            switch (c) {
+            case 'k':
+                keyfile = optarg;
+                break;
+            case 'm':
+                message_file = optarg;
+                break;
+            case 'c':
+                ciphertext_file = optarg;
+                break;
+            case '?':
+                abort();
+            }
+        }
+    }
+
+    if (!keyfile) {
+        fprintf(stderr, "Keyfile must be specified with -k.\n");
+        exit(1);
+    }
+
+    if (!message_file) {
+        fprintf(stderr, "Message file must be specified with -m.\n");
+        exit(1);
+    }
+
+    if (!ciphertext_file) {
+        fprintf(stderr, "Output keyfile must be specified with -k.\n");
+        exit(1);
+    }
+
+    uint8_t k[crypto_box_BEFORENMBYTES];
+
+    load_key(keyfile, "beforenm ", sizeof k, k);
+
+    int message_fd = open(message_file, O_RDONLY);
+    fatal(message_fd, "open");
+
+    size_t mlen;
+    void *m = load_file(message_fd, crypto_box_ZEROBYTES, &mlen);
+    fatal(close(message_fd), "close");
+
+    uint8_t c[crypto_box_NONCEBYTES + mlen];
+
+    randombytes(c, crypto_box_NONCEBYTES);
+
+    crypto_box_afternm(&c[crypto_box_NONCEBYTES], m, mlen, c, k);
+
+    int ciphertext_fd = open(ciphertext_file, O_WRONLY|O_CREAT, public_mode);
+    fatal(ciphertext_fd, "open");
+
+    // TODO: This unnecessarily includes the zero bytes.
+    fatal(write(ciphertext_fd, c, sizeof c), "write");
+    fatal(close(ciphertext_fd), "close");
+
+    free(m);
+}
+
+void cmd_box_open_afternm(int argc, char *argv[argc]) {
+    const char *keyfile = NULL;
+    const char *message_file = NULL;
+    const char *ciphertext_file = NULL;
+
+    {
+    char c;
+    while ((c = getopt(argc, argv, "m:c:k:")) != -1) {
+        switch (c) {
+        case 'k':
+            keyfile = optarg;
+            break;
+        case 'm':
+            message_file = optarg;
+            break;
+        case 'c':
+            ciphertext_file = optarg;
+            break;
+        case '?':
+            abort();
+        }
+    }
+    }
+
+    if (!keyfile) {
+        fprintf(stderr, "Keyfile must be specified with -k.\n");
+        exit(1);
+    }
+
+    if (!message_file) {
+        fprintf(stderr, "Message file must be specified with -m.\n");
+        exit(1);
+    }
+
+    if (!ciphertext_file) {
+        fprintf(stderr, "Ciphertext file must be specified with -c.\n");
+        exit(1);
+    }
+
+    uint8_t k[crypto_box_BEFORENMBYTES];
+
+    load_key(keyfile, "beforenm ", sizeof k, k);
+
+    int ciphertext_fd = open(ciphertext_file, O_RDONLY);
+    fatal(ciphertext_fd, "open");
+
+    size_t full_clen;
+    uint8_t *c = load_file(ciphertext_fd, 0, &full_clen);
+    fatal(close(ciphertext_fd), "close");
+
+    // TODO: check this doesn't underflow
+    size_t clen = full_clen - crypto_box_NONCEBYTES;
+
+    uint8_t m[clen];
+
+    // TODO: Ideally change this so it doesn't include the unnecessary zeroes
+    //       but until then it should at least make sure the zero bytes are
+    //       cleared.
+    if (crypto_box_open_afternm(m, &c[crypto_box_NONCEBYTES], clen, c, k) == -1) {
+        fprintf(stderr, "open failed!\n");
+        exit(1);
+    }
+
+    int message_fd = open(message_file, O_WRONLY|O_CREAT, S_IRUSR);
     fatal(message_fd, "open");
 
     // TODO: This unnecessarily includes the zero bytes.
@@ -349,17 +493,29 @@ void cmd_box_open(int argc, char *argv[argc]) {
 }
 
 void cmd_help(int argc, char *argv[argc]) {
-    fprintf(stderr, "nacl box keypair\n");
+    fprintf(stderr, "Usage:\n");
+    fprintf(stderr, " nacl box-keypair -p PUBLIC -s SECRET\n");
+    fprintf(stderr, " nacl box -p PUBLIC -s SECRET -m MESSAGE -c CIPHERTEXT\n");
+    fprintf(stderr, " nacl box-open -p PUBLIC -s SECRET -c CIPHERTEXT -m MESSAGE\n");
+    fprintf(stderr, " nacl box-beforenm -p PUBLIC -s SECRET -k KEYFILE\n");
+    fprintf(stderr, " nacl box-afternm -k KEYFILE -m MESSAGE -c CIPHERTEXT\n");
+    fprintf(stderr, " nacl box-open-afternm -k KEYFILE -c CIPHERTEXT -m MESSAGE\n");
     exit(1);
 }
 
 int main(int argc, char *argv[argc]) {
-    if (argc >= 3 && strcmp(argv[1], "box") == 0 && strcmp(argv[2], "keypair") == 0) {
-        cmd_box_keypair(argc - 2, &argv[2]);
-    } else if (argc >= 3 && strcmp(argv[1], "box") == 0 && strcmp(argv[2], "make") == 0) {
-        cmd_box_make(argc - 2, &argv[2]);
-    } else if (argc >= 3 && strcmp(argv[1], "box") == 0 && strcmp(argv[2], "open") == 0) {
-        cmd_box_open(argc - 2, &argv[2]);
+    if (argc >= 2 && strcmp(argv[1], "box-keypair") == 0) {
+        cmd_box_keypair(argc - 1, &argv[1]);
+    } else if (argc >= 2 && strcmp(argv[1], "box") == 0) {
+        cmd_box(argc - 1, &argv[1]);
+    } else if (argc >= 2 && strcmp(argv[1], "box-open") == 0) {
+        cmd_box_open(argc - 1, &argv[1]);
+    } else if (argc >= 2 && strcmp(argv[1], "box-beforenm") == 0) {
+        cmd_box_beforenm(argc - 1, &argv[1]);
+    } else if (argc >= 2 && strcmp(argv[1], "box-afternm") == 0) {
+        cmd_box_afternm(argc - 1, &argv[1]);
+    } else if (argc >= 2 && strcmp(argv[1], "box-open-afternm") == 0) {
+        cmd_box_open_afternm(argc - 1, &argv[1]);
     } else {
         cmd_help(argc, argv);
     }
