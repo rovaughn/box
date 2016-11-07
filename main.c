8633737050c4dcaa24132f5309a5c57fd5e1a421
Alec NEwman <alecnwmn904@gmail.com>
2016-11-06 19:56:49 -0500

    Works, basically.

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <getopt.h>
 #include <stdbool.h>
 #include "readpass.h"
+#include "sqlite3.h"
 
 #define range(i, a, b) for (i = a; i < b; i++)
 #define PACKED __attribute__((__packed__))
@@ -53,30 +54,26 @@ void fatal(int err, const char *message) {
     }
 }
 
-void load_contact(const char *name, uint8_t pk[crypto_box_PUBLICKEYBYTES]) {
-}
-
-void load_identity(const char *name, uint8_t pk[crypto_box_PUBLICKEYBYTES], uint8_t sk[crypto_box_SECRETKEYBYTES]) {
-}
-
-void load_sign_identity(const char *name, uint8_t pk[crypto_sign_PUBLICKEYBYTES], uint8_t sk[crypto_sign_SECRETKEYBYTES]) {
-}
-
-void load_identity_by_pk(uint8_t pk[crypto_sign_PUBLICKEYBYTES], uint8_t sk[crypto_sign_SECRETKEYBYTES]) {
+void fatalfile(int err, const char *filename, const char *message) {
+    if (err == -1) {
+        fprintf(stderr, "%s: ", filename);
+        perror(message);
+        exit(1);
+    }
 }
 
 #define load_file_initial_capacity (1<<14)
 #define load_file_min_read (1<<14)
 
-void *load_file(int fd, size_t *size) {
+void *read_all(int fd, size_t *size) {
     size_t filled = 0;
     size_t capacity = load_file_initial_capacity;
-    uint8_t *buffer = malloc(capacity);
+    uint8_t *buffer = malloc(capacity + 1); // extra byte for \0
 
     for (;;) {
         while (capacity - filled < load_file_min_read) {
             capacity *= 2;
-            buffer = realloc(buffer, capacity);
+            buffer = realloc(buffer, capacity + 1); // extra byte for \0
         }
 
         ssize_t nr = read(fd, &buffer[filled], capacity - filled);
@@ -91,9 +88,116 @@ void *load_file(int fd, size_t *size) {
         }
     }
 
-    *size = filled;
+    buffer[filled] = '\0';
+
+    if (size != NULL) { *size = filled; }
 
     return buffer;
+}
+
+void read_exactly(int fd, size_t len, uint8_t buf[len]) {
+    size_t filled = 0;
+
+    for (;;) {
+        int nr = read(fd, &buf[filled], len - filled);
+
+        if (nr < 0) {
+            perror("read exactly");
+            exit(1);
+        } else if (nr == 0 && filled < len) {
+            fprintf(stderr, "Tried to fill buffer with %zu bytes but only got %zu\n", len, filled);
+            exit(1);
+        } else {
+            filled += nr;
+
+            if (filled == len) {
+                return;
+            }
+        }
+    }
+}
+
+void fataldb(sqlite3 *db, const char *message, int rc) {
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "%s: %s\n", message, sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(1);
+    }
+}
+
+#define fataldb(rc) if ((rc) != SQLITE_OK) { fprintf(stderr, "Line #%d: %s\n", __LINE__, sqlite3_errmsg(db)); sqlite3_close(db); exit(1); }
+
+// databse is at the first valid location of the following:
+//  - ${BOX_HOME}
+//  - ${HOME}/.box-home
+//  - .box-home
+sqlite3 *use_db() {
+    static sqlite3 *db = NULL;
+
+    if (!db) {
+        char *box_home = getenv("BOX_HOME");
+
+        if (!box_home) {
+            const char *home = getenv("HOME");
+
+            if (!home) {
+                box_home = ".box-home";
+            } else {
+                box_home = malloc(strlen(home) + strlen("/.box-home") + 1);
+                strcpy(box_home, home);
+                strcat(box_home, "/.box-home");
+            }
+        }
+
+        if (sqlite3_open_v2(box_home, &db, SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE, NULL)) {
+            fprintf(stderr, "Opening database: %s\n", sqlite3_errmsg(db));
+            sqlite3_close(db);
+            exit(1);
+        }
+
+        {
+            sqlite3_stmt *stmt;
+            fataldb(sqlite3_prepare_v2(db, "pragma user_version;", -1, &stmt, NULL));
+            if (sqlite3_step(stmt) != SQLITE_ROW) {
+                fprintf(stderr, "Could not get user_version.\n");
+                exit(1);
+            }
+            int version = sqlite3_column_int(stmt, 0);
+            fataldb(sqlite3_finalize(stmt));
+
+            if (version == 0) {
+                fataldb(sqlite3_exec(db,
+                    "CREATE TABLE identity ("
+                    "   name text,"
+                    "   boxpk blob,"
+                    "   boxsk blob,"
+                    "   signpk blob,"
+                    "   signsk blob"
+                    ");"
+                    "CREATE TABLE contact ("
+                    "   name text,"
+                    "   boxpk blob,"
+                    "   signpk blob"
+                    ");"
+                    "PRAGMA user_version = 1;",
+                    NULL, NULL, NULL
+                ));
+            } else if (version > 1) {
+                fprintf(stderr, "Unknown database version %d\n", version);
+                exit(1);
+            }
+        }
+    }
+
+    return db;
+}
+
+void *load_all(const char *filename, size_t *size) {
+    int fd = open(filename, O_RDONLY);
+    fatalfile(fd, filename, "open");
+    void *buf = read_all(fd, size);
+    fatalfile(close(fd), filename, "close");
+    return buf;
 }
 
 const char *bin_name;
@@ -153,6 +257,107 @@ typedef struct PACKED {
     uint8_t c[];
 } box_from_to_header;
 
+__attribute__((noreturn)) void cmd_new_identity(int argc, char *argv[argc]) {
+    if (argc != 1) {
+        usage();
+    }
+
+    const char *name = argv[0];
+
+    uint8_t boxpk[crypto_box_PUBLICKEYBYTES];
+    uint8_t boxsk[crypto_box_SECRETKEYBYTES];
+    crypto_box_keypair(boxpk, boxsk);
+
+    uint8_t signpk[crypto_sign_PUBLICKEYBYTES];
+    uint8_t signsk[crypto_sign_SECRETKEYBYTES];
+    crypto_sign_keypair(signpk, signsk);
+
+    sqlite3 *db = use_db();
+
+    sqlite3_stmt *stmt;
+
+    fataldb(sqlite3_prepare_v2(
+        db,
+        "INSERT INTO identity (name, boxpk, boxsk, signpk, signsk) "
+        "VALUES (?, ?, ?, ?, ?)",
+        -1, &stmt, NULL
+    ));
+
+    fataldb(sqlite3_bind_text(stmt, 1, name, strlen(name), SQLITE_STATIC));
+    fataldb(sqlite3_bind_blob(stmt, 2, boxpk, sizeof boxpk, SQLITE_STATIC));
+    fataldb(sqlite3_bind_blob(stmt, 3, boxsk, sizeof boxsk, SQLITE_STATIC));
+    fataldb(sqlite3_bind_blob(stmt, 4, signpk, sizeof signpk, SQLITE_STATIC));
+    fataldb(sqlite3_bind_blob(stmt, 5, signsk, sizeof signsk, SQLITE_STATIC));
+
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        fprintf(stderr, "%s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(1);
+    }
+
+    fataldb(sqlite3_finalize(stmt));
+
+    char boxpkhex[1+2*sizeof boxpk];
+    char signpkhex[1+2*sizeof signpk];
+
+    sodium_bin2hex(boxpkhex, sizeof boxpkhex, boxpk, sizeof boxpk);
+    sodium_bin2hex(signpkhex, sizeof signpkhex, signpk, sizeof signpk);
+
+    printf("%s %s %s\n", name, boxpkhex, signpkhex);
+    exit(0);
+}
+
+__attribute__((noreturn)) void cmd_add_contact(int argc, char *argv[argc]) {
+    if (argc != 3) {
+        usage();
+    }
+
+    const char *name = argv[0];
+    const char *hexboxpk = argv[1];
+    const char *hexsignpk = argv[2];
+
+    if (strlen(hexboxpk)/2 != crypto_box_PUBLICKEYBYTES) {
+        fprintf(stderr, "Invalid public key.\n");
+        exit(1);
+    }
+
+    if (strlen(hexsignpk)/2 != crypto_sign_PUBLICKEYBYTES) {
+        fprintf(stderr, "Invalid public key.\n");
+        exit(1);
+    }
+
+
+    uint8_t boxpk[crypto_box_PUBLICKEYBYTES];
+    uint8_t signpk[crypto_sign_PUBLICKEYBYTES];
+
+    sodium_hex2bin(boxpk, sizeof boxpk, hexboxpk, strlen(hexboxpk), NULL, NULL, NULL);
+    sodium_hex2bin(signpk, sizeof signpk, hexsignpk, strlen(hexsignpk), NULL, NULL, NULL);
+
+    sqlite3 *db = use_db();
+
+    sqlite3_stmt *stmt;
+
+    fataldb(sqlite3_prepare_v2(
+        db,
+        "INSERT INTO contact (name, boxpk, signpk) "
+        "VALUES (?, ?, ?)",
+        -1, &stmt, NULL
+    ));
+
+    fataldb(sqlite3_bind_text(stmt, 1, name, strlen(name), SQLITE_STATIC));
+    fataldb(sqlite3_bind_blob(stmt, 2, boxpk, sizeof boxpk, SQLITE_STATIC));
+    fataldb(sqlite3_bind_blob(stmt, 3, signpk, sizeof signpk, SQLITE_STATIC));
+
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        fprintf(stderr, "%s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        exit(1);
+    }
+
+    fataldb(sqlite3_finalize(stmt));
+    exit(0);
+}
+
 __attribute__((noreturn)) void cmd_seal(int argc, char *argv[argc]) {
     bool use_password = false;
     char *to = NULL;
@@ -192,7 +397,7 @@ __attribute__((noreturn)) void cmd_seal(int argc, char *argv[argc]) {
         }
 
         size_t mlen;
-        void *m = load_file(STDIN_FILENO, &mlen);
+        void *m = read_all(STDIN_FILENO, &mlen);
 
         size_t clen = crypto_secretbox_MACBYTES + mlen;
         size_t boxlen = sizeof(box_password_header) + clen;
@@ -219,7 +424,7 @@ __attribute__((noreturn)) void cmd_seal(int argc, char *argv[argc]) {
         fprintf(stderr, "Encrypting message...\n");
         crypto_secretbox_easy(box->c, m, mlen, box->nonce, k);
 
-        fprintf(stderr, "Writing out...\n");
+        fprintf(stderr, "Writing out %zu bytes...\n", boxlen);
         fatal(write(STDOUT_FILENO, box, boxlen), "write");
         exit(0);
     } else if (to && from && !use_password) {
@@ -229,22 +434,58 @@ __attribute__((noreturn)) void cmd_seal(int argc, char *argv[argc]) {
         }
 
         size_t mlen;
-        void *m = load_file(STDIN_FILENO, &mlen);
+        void *m = read_all(STDIN_FILENO, &mlen);
 
         size_t clen = crypto_box_MACBYTES + mlen;
         size_t boxlen = sizeof(box_from_to_header) + clen;
         box_from_to_header *box = malloc(boxlen);
 
-        uint8_t sender_sk[crypto_box_SECRETKEYBYTES];
-
         save_uint64(box->header.len, boxlen);
         save_uint8(box->header.type, BOX_FROM_TO);
-        load_contact(to, box->receiver_pk);
-        load_identity(from, box->sender_pk, sender_sk);
+
+        sqlite3 *db = use_db();
+        sqlite3_stmt *stmt;
+
+        fataldb(sqlite3_prepare_v2(db, "SELECT boxpk FROM contact WHERE name = ?;", -1, &stmt, NULL));
+        fataldb(sqlite3_bind_text(stmt, 1, to, strlen(to), SQLITE_STATIC));
+        switch (sqlite3_step(stmt)) {
+        case SQLITE_DONE:
+            fprintf(stderr, "Unknown contact %s\n", to);
+            exit(1);
+        case SQLITE_ROW:
+            break;
+        default:
+            fataldb(1);
+        }
+
+        // TODO: Should check size and columns
+        const void *receiver_pk = sqlite3_column_blob(stmt, 0);
+        memcpy(box->receiver_pk, receiver_pk, sizeof box->receiver_pk);
+        fataldb(sqlite3_finalize(stmt));
+
+        fataldb(sqlite3_prepare_v2(db, "SELECT boxpk, boxsk FROM identity WHERE name = ?;", -1, &stmt, NULL));
+        fataldb(sqlite3_bind_text(stmt, 1, from, strlen(from), SQLITE_STATIC));
+        switch (sqlite3_step(stmt)) {
+        case SQLITE_DONE:
+            fprintf(stderr, "Unknown identity %s\n", to);
+            exit(1);
+        case SQLITE_ROW:
+            break;
+        default:
+            fataldb(1);
+        }
+
+        // TODO: Should check size and columns
+        const void *sender_pk = sqlite3_column_blob(stmt, 0);
+        const void *sender_sk = sqlite3_column_blob(stmt, 1);
+        memcpy(box->sender_pk, sender_pk, sizeof box->sender_pk);
+
         randombytes_buf(box->nonce, sizeof box->nonce);
         
         fatal(crypto_box_easy(box->c, m, mlen, box->nonce, box->receiver_pk, sender_sk), "crypto_box_easy");
+        fataldb(sqlite3_finalize(stmt));
 
+        fprintf(stderr, "Writing out %zu bytes...\n", boxlen);
         fatal(write(STDOUT_FILENO, box, boxlen), "write");
         exit(0);
     } else if (to && !from && !use_password) {
@@ -254,7 +495,7 @@ __attribute__((noreturn)) void cmd_seal(int argc, char *argv[argc]) {
         }
 
         size_t mlen;
-        void *m = load_file(STDIN_FILENO, &mlen);
+        void *m = read_all(STDIN_FILENO, &mlen);
 
         size_t clen = crypto_box_SEALBYTES + mlen;
         size_t boxlen = sizeof(box_to_header) + clen;
@@ -262,9 +503,30 @@ __attribute__((noreturn)) void cmd_seal(int argc, char *argv[argc]) {
 
         save_uint64(box->header.len, boxlen);
         save_uint8(box->header.type, BOX_TO);
-        load_contact(to, box->receiver_pk);
+
+        sqlite3 *db = use_db();
+        sqlite3_stmt *stmt;
+
+        fataldb(sqlite3_prepare_v2(db, "SELECT boxpk FROM contact WHERE name = ?;", -1, &stmt, NULL));
+        fataldb(sqlite3_bind_text(stmt, 1, to, strlen(to), SQLITE_STATIC));
+        switch (sqlite3_step(stmt)) {
+        case SQLITE_DONE:
+            fprintf(stderr, "Unknown contact %s\n", to);
+            exit(1);
+        case SQLITE_ROW:
+            break;
+        default:
+            fataldb(1);
+        }
+
+        // TODO: Should check size and columns
+        const void *receiver_pk = sqlite3_column_blob(stmt, 0);
+        memcpy(box->receiver_pk, receiver_pk, sizeof box->receiver_pk);
+        fataldb(sqlite3_finalize(stmt));
+
         crypto_box_seal(box->c, m, mlen, box->receiver_pk);
 
+        fprintf(stderr, "Writing out %zu bytes...\n", boxlen);
         fatal(write(STDOUT_FILENO, box, boxlen), "write");
         exit(0);
     } else if (from && !to && !use_password) {
@@ -274,45 +536,43 @@ __attribute__((noreturn)) void cmd_seal(int argc, char *argv[argc]) {
         }
 
         size_t mlen;
-        void *m = load_file(STDIN_FILENO, &mlen);
+        void *m = read_all(STDIN_FILENO, &mlen);
 
         size_t smlen = crypto_sign_BYTES + mlen;
         size_t boxlen = sizeof(box_from_header) + smlen;
         box_from_header *box = malloc(boxlen);
 
-        uint8_t sender_sk[crypto_sign_SECRETKEYBYTES];
-
         save_uint64(box->header.len, boxlen);
         save_uint8(box->header.type, BOX_FROM);
-        load_sign_identity(from, box->sender_pk, sender_sk);
-        crypto_sign(box->sm, NULL, m, mlen, sender_sk);
 
+        sqlite3 *db = use_db();
+        sqlite3_stmt *stmt;
+
+        fataldb(sqlite3_prepare_v2(db, "SELECT signpk, signsk FROM identity WHERE name = ?;", -1, &stmt, NULL));
+        fataldb(sqlite3_bind_text(stmt, 1, from, strlen(from), SQLITE_STATIC));
+        switch (sqlite3_step(stmt)) {
+        case SQLITE_DONE:
+            fprintf(stderr, "Unknown identity %s\n", to);
+            exit(1);
+        case SQLITE_ROW:
+            break;
+        default:
+            fataldb(1);
+        }
+
+        // TODO: Should check size and columns
+        const void *sender_pk = sqlite3_column_blob(stmt, 0);
+        const void *sender_sk = sqlite3_column_blob(stmt, 1);
+        memcpy(box->sender_pk, sender_pk, sizeof box->sender_pk);
+
+        crypto_sign(box->sm, NULL, m, mlen, sender_sk);
+        fataldb(sqlite3_finalize(stmt));
+
+        fprintf(stderr, "Writing out %zu bytes...\n", boxlen);
         fatal(write(STDOUT_FILENO, box, boxlen), "write");
         exit(0);
     } else {
         usage();
-    }
-}
-
-void read_exactly(int fd, size_t len, uint8_t buf[len]) {
-    size_t filled = 0;
-
-    for (;;) {
-        int nr = read(fd, &buf[filled], len - filled);
-
-        if (nr < 0) {
-            perror("read exactly");
-            exit(1);
-        } else if (nr == 0) {
-            if (filled != len) {
-                fprintf(stderr, "Didn't read the expected number of bytes.\n");
-                exit(1);
-            }
-
-            return;
-        } else {
-            filled += nr;
-        }
     }
 }
 
@@ -324,7 +584,7 @@ __attribute__((noreturn)) void cmd_open() {
 
     box_common_header header;
 
-    fatal(read(STDIN_FILENO, &header, sizeof header), "read header");
+    read_exactly(STDIN_FILENO, sizeof header, (uint8_t*)&header);
 
     uint8_t boxtype = read_uint8(header.type);
     size_t boxlen = read_uint64(header.len);
@@ -377,8 +637,24 @@ __attribute__((noreturn)) void cmd_open() {
 
         box_from_to_header *box = (box_from_to_header*)raw_box;
 
-        uint8_t receiver_sk[crypto_box_SECRETKEYBYTES];
-        load_identity_by_pk(box->receiver_pk, receiver_sk);
+        sqlite3 *db = use_db();
+        sqlite3_stmt *stmt;
+
+        fataldb(sqlite3_prepare_v2(db, "SELECT name, boxsk FROM identity WHERE boxpk = ?;", -1, &stmt, NULL));
+        fataldb(sqlite3_bind_blob(stmt, 1, box->receiver_pk, sizeof box->receiver_pk, SQLITE_STATIC));
+        switch (sqlite3_step(stmt)) {
+        case SQLITE_DONE:
+            fprintf(stderr, "Box matches no known identity.\n");
+            exit(1);
+        case SQLITE_ROW:
+            break;
+        default:
+            fataldb(1);
+        }
+
+        // TODO: Should check size and columns
+        //const unsigned char *name = sqlite3_column_text(stmt, 0);
+        const void *receiver_sk = sqlite3_column_blob(stmt, 1);
 
         // TODO: Verify identity with contacts.
         size_t clen = boxlen - sizeof(box_from_to_header);
@@ -399,8 +675,24 @@ __attribute__((noreturn)) void cmd_open() {
 
         box_to_header *box = (box_to_header*)raw_box;
 
-        uint8_t receiver_sk[crypto_box_SECRETKEYBYTES];
-        load_identity_by_pk(box->receiver_pk, receiver_sk);
+        sqlite3 *db = use_db();
+        sqlite3_stmt *stmt;
+
+        fataldb(sqlite3_prepare_v2(db, "SELECT name, boxsk FROM identity WHERE boxpk = ?;", -1, &stmt, NULL));
+        fataldb(sqlite3_bind_blob(stmt, 1, box->receiver_pk, sizeof box->receiver_pk, SQLITE_STATIC));
+        switch (sqlite3_step(stmt)) {
+        case SQLITE_DONE:
+            fprintf(stderr, "Box matches no known identity.\n");
+            exit(1);
+        case SQLITE_ROW:
+            break;
+        default:
+            fataldb(1);
+        }
+
+        // TODO: Should check size and columns
+        //const unsigned char *name = sqlite3_column_text(stmt, 0);
+        const void *receiver_sk = sqlite3_column_blob(stmt, 1);
 
         size_t clen = boxlen - sizeof(box_to_header);
         size_t mlen = clen - crypto_box_SEALBYTES;
@@ -419,6 +711,24 @@ __attribute__((noreturn)) void cmd_open() {
         }
 
         box_from_header *box = (box_from_header*)raw_box;
+
+        sqlite3 *db = use_db();
+        sqlite3_stmt *stmt;
+
+        fataldb(sqlite3_prepare_v2(db, "SELECT name FROM contact WHERE signpk = ?;", -1, &stmt, NULL));
+        fataldb(sqlite3_bind_blob(stmt, 1, box->sender_pk, sizeof box->sender_pk, SQLITE_STATIC));
+        switch (sqlite3_step(stmt)) {
+        case SQLITE_DONE:
+            fprintf(stderr, "Box matches no known identity.\n");
+            exit(1);
+        case SQLITE_ROW:
+            break;
+        default:
+            fataldb(1);
+        }
+
+        // TODO: Should check size and columns
+        //const unsigned char *name = sqlite3_column_text(stmt, 0);
 
         size_t smlen = boxlen - sizeof(box_from_header);
         size_t mlen = smlen - crypto_sign_BYTES;
@@ -443,18 +753,14 @@ int main(int argc, char *argv[argc]) {
 
     if (argc >= 2 && strcmp(argv[1], "seal") == 0) {
         cmd_seal(argc - 2, &argv[2]);
-        return 0;
-    } else if (argc == 2 && strcmp(argv[1], "open") == 0) {
+    } else if (argc >= 2 && strcmp(argv[1], "open") == 0) {
         cmd_open(argc - 2, &argv[2]);
-        return 0;
-    /*} else if (argc == 2 && strcmp(argv[1], "add-contact") == 0) {
-        cmd_add_contact(argc - 2, &argv[2]);
-    } else if (argc == 2 && strcmp(argv[1], "list-contacts") == 0) {
-        cmd_list_contacts(argc - 2, &argv[2]);
-    } else if (argc == 2 && strcmp(argv[1], "new-identity") == 0) {
+    } else if (argc >= 2 && strcmp(argv[1], "new-identity") == 0) {
         cmd_new_identity(argc - 2, &argv[2]);
-    } else if (argc == 2 && strcmp(argv[1], "list-identities") == 0) {
-        cmd_get_identity(argc - 2, &argv[2]);*/
+    /*} else if (argc == 2 && strcmp(argv[1], "list-contacts") == 0) {
+        cmd_list_contacts(argc - 2, &argv[2]);*/
+    } else if (argc >= 2 && strcmp(argv[1], "add-contact") == 0) {
+        cmd_add_contact(argc - 2, &argv[2]);
     } else {
         usage();
     }
--- makefile
+++ makefile
@@ -1,25 +1,55 @@
 
-CFLAGS := -Wall -Werror -I/usr/local/include
-LDFLAGS := -lsodium -L/usr/local/lib
-SRC := main.c readpass.c insecure_memzero.c warnp.c
-export CC := musl-gcc
+sodium_ver := 1.0.11
 
-all: box
+all: testlog
 
-libsodium-1.0.11.tar.gz:
-	wget 'https://download.libsodium.org/libsodium/releases/libsodium-1.0.11.tar.gz'
+testlog: box.debug test
+	./test 2>&1 | tee testlog
 
-libsodium-1.0.11: libsodium-1.0.11.tar.gz
-	tar xaf libsodium-1.0.11.tar.gz
+libsodium-${sodium_ver}.tar.gz:
+	wget 'https://download.libsodium.org/libsodium/releases/libsodium-${sodium_ver}.tar.gz'
 
-.libsodium: libsodium-1.0.11
-	cd libsodium-1.0.11 && ./configure
-	cd libsodium-1.0.11 && make
-	cd libsodium-1.0.11 && sudo make install
-	touch .libsodium
+libsodium.musl: libsodium-${sodium_ver}.tar.gz
+	rm -rf libsodium-${sodium_ver}
+	tar xaf libsodium-${sodium_ver}.tar.gz
+	cd libsodium-${sodium_ver} && CC=musl-gcc ./configure --prefix=$(shell readlink -f $@)
+	cd libsodium-${sodium_ver} && make
+	cd libsodium-${sodium_ver} && make install
+	rm -rf libsodium-${sodium_ver}
 
-box: main.c .libsodium
-	${CC} ${CFLAGS} -O3 -static -flto ${SRC} ${LDFLAGS} -o box
+libsodium: libsodium-${sodium_ver}.tar.gz
+	rm -rf libsodium-${sodium_ver}
+	tar xaf libsodium-${sodium_ver}.tar.gz
+	cd libsodium-${sodium_ver} && ./configure --prefix=$(shell readlink -f $@)
+	cd libsodium-${sodium_ver} && make
+	cd libsodium-${sodium_ver} && make install
+	rm -rf libsodium-${sodium_ver}
+
+sqlite-autoconf-3150100.tar.gz:
+	wget 'https://www.sqlite.org/2016/sqlite-autoconf-3150100.tar.gz'
+
+sqlite-autoconf-3150100: sqlite-autoconf-3150100.tar.gz
+	tar xaf $^
+
+sqlite3.c: sqlite-autoconf-3150100
+	ln -fs $^/sqlite3.c .
+
+sqlite3.h: sqlite-autoconf-3150100
+	ln -fs $^/sqlite3.h .
+
+box.debug: main.c libsodium sqlite3.c sqlite3.h
+	gcc -Wall -Werror -g main.c readpass.c insecure_memzero.c warnp.c sqlite3.c \
+		-lsodium -lpthread -ldl -Ilibsodium/include -Llibsodium/lib -o $@
+
+box: main.c libsodium.musl sqlite3.c sqlite3.h
+	musl-gcc -Wall -Werror -I/usr/local/include -O3 -static -flto \
+		     main.c readpass.c insecure_memzero.c warnp.c sqlite3.c \
+		     -Ilibsodium.musl/include -lsodium -Llibsodium.musl/lib -o $@
 	strip -s box
 	ls -lah box
+
+clean:
+	rm -rf libsodium-${sodium_ver}.tar.gz libsodium.musl libsodium \
+		   sqlite-autoconf-3150100.tar.gz sqlite3 sqlite3.h box box.debug \
+		   testlog
 
